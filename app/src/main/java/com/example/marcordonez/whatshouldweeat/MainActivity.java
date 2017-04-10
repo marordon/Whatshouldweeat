78e7a5ed276c5af8ffab509e1c74005a6a7a75fc
marordon <marc.ordonez@gmail.com>
2017-04-10 13:29:17 -0500

    clean up

--- app/src/main/java/com/example/marcordonez/whatshouldweeat/MainActivity.java
+++ app/src/main/java/com/example/marcordonez/whatshouldweeat/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.marcordonez.whatshouldweeat;
-
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
@@ -17,14 +15,11 @@ import android.Manifest;
 import android.location.LocationManager;
 import android.location.Location;
 import android.content.Context;
-import android.location.Criteria;
 import android.os.IBinder;
 import android.app.Service;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,58 +28,11 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
-
 import android.util.Log;
-
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.common.api.ResultCallback;
-
-import android.text.TextUtils;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceBuffer;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.location.places.AutocompletePrediction;
-import java.util.List;
-import java.util.ArrayList;
-import com.google.android.gms.location.places.AutocompleteFilter;
-import com.google.android.gms.location.places.AutocompletePredictionBuffer;
-import java.util.concurrent.TimeUnit;
-import com.google.android.gms.common.api.Status;
-import java.util.Iterator;
-import com.google.android.gms.location.places.PlaceFilter;
-import android.text.Html;
-import android.net.Uri;
-import android.text.Spanned;
-import android.content.res.Resources;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
@@ -94,55 +42,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private GoogleApiClient mGoogleApiClient;
     private int PLACE_PICKER_REQUEST = 1;
     String url="";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        food = (Button) findViewById(R.id.Food);
-        whatYouWant = (TextView) findViewById(R.id.WhatYouWant);
-        weblist = (TextView) findViewById(R.id.weblist);
-
-
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .enableAutoManage(this, 0, this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-            //whatYouWant.setText( "nope");
-
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (mGoogleApiClient != null)
-            mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-        super.onStop();
-    }
+    String longi;
+    String lat;
     public class GPSTracker extends Service implements LocationListener {
 
         private final Context mContext;
@@ -164,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
         // The minimum time between updates in milliseconds
-        private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+        private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 5; // 1 minute
 
         // Declaring a Location Manager
         protected LocationManager locationManager;
@@ -300,6 +201,60 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         }
 
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        food = (Button) findViewById(R.id.Food);
+        whatYouWant = (TextView) findViewById(R.id.WhatYouWant);
+        weblist = (TextView) findViewById(R.id.weblist);
+        GPSTracker gps = new GPSTracker(this);
+        if(gps.canGetLocation()) {
+            double latitude = gps.getLatitude(); // returns latitude
+            double longitude = gps.getLongitude(); // returns longitude
+            longi = Double.toString(longitude);
+            lat = Double.toString(latitude);
+        }
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, 0, this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            //whatYouWant.setText( "nope");
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
     public void onClickFood(View view) {
         Random randomGenerator = new Random();
         int rndm=randomGenerator.nextInt(17);
@@ -361,31 +316,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         weblist.setText(ftype);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         //LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
        // Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         //double longitude = location.getLongitude();
         //double latitude = location.getLatitude();
-        GPSTracker gps = new GPSTracker(this);
-        if(gps.canGetLocation()){
-            double latitude= gps.getLatitude(); // returns latitude
-            double longitude = gps.getLongitude(); // returns longitude
-            String longi =Double.toString(longitude);
-            String lat= Double.toString(latitude);
+if(lat == null || longi == null){
 
             url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
             url=url.concat(String.valueOf(lat));
@@ -398,106 +334,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             //whatYouWant.setText(url);
         }
         else{
-            whatYouWant.setText("Could not access location");
+
+    GPSTracker gps = new GPSTracker(this);
+    if(gps.canGetLocation()) {
+        double latitude = gps.getLatitude(); // returns latitude
+        double longitude = gps.getLongitude(); // returns longitude
+        longi = Double.toString(longitude);
+        lat = Double.toString(latitude);
+        whatYouWant.setText("Loading...");
+
+        url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+        url=url.concat(String.valueOf(lat));
+        url=url.concat(",");
+        url=url.concat(String.valueOf(longi));
+        url=url.concat("&rankby=distance&type=restaurant&key=AIzaSyBDf3cLEXwV77wvfihpvNbsnqDOixWD4Kc&opennow&keyword=");
+        url=url.concat(ftype);
+        //url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.58576431,-101.87939933&radius=500&type=restaurant&key=AIzaSyBDf3cLEXwV77wvfihpvNbsnqDOixWD4Kc";
+        new Dtask().execute(url);
+    }
+    else{
+        whatYouWant.setText("Could not access location");
+    }
         }
 
 
-
-        //guessCurrentPlace();
-
     }
 
-    private void guessCurrentPlace() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
 
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-            //whatYouWant.setText( "nope");
-            return;
-        }
-        /*PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
-        result.setResultCallback( new ResultCallback<PlaceLikelihoodBuffer>() {
-            @Override
-            public void onResult( PlaceLikelihoodBuffer likelyPlaces ) {
-                whatYouWant.setText( "" );
-                int c=likelyPlaces.getCount();
-                PlaceLikelihood placeLikelihood = likelyPlaces.get( 0 );
-                String content[] =  new String[c];
-
-                ArrayList<String> filter = new ArrayList<>();
-                filter.add(""+Place.TYPE_FOOD);
-
-               PlaceFilter placeFilter = new PlaceFilter(true,filter);
-                StringBuilder builder = new StringBuilder();
-                for (String str : placeFilter.getPlaceIds () ) {
-                    builder.append(str).append(" ");
-                }
-
-                String txt = builder.toString();
-
-
-                whatYouWant.setText(txt  );
-
-                for(int i=0; i<c; i++){
-                    content[i]=""+placeLikelihood.getPlace().getName();
-                    //whatYouWant.append( content[i]+""+(int) ( placeLikelihood.getLikelihood() * 100 )+"\n" );
-                }
-
-/*
-                PlaceLikelihood placeLikelihood = likelyPlaces.get( 0 );
-                String content = "";
-                if( placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty( placeLikelihood.getPlace().getName() ) )
-                    content = "Most likely place: " + placeLikelihood.getPlace().getName() + "\n";
-                if( placeLikelihood != null )
-                    content += "Percent change of being there: " + (int) ( placeLikelihood.getLikelihood() * 100 ) + "%";
-                whatYouWant.setText( content );
-*/
-        //    likelyPlaces.release();
-        //}
-        //});
-
-        ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback = new ResultCallback<PlaceBuffer>() {
-            @Override
-            public void onResult(PlaceBuffer places) {
-                if (!places.getStatus().isSuccess()) {
-                    // Request did not complete successfully
-                    //Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
-                    places.release();
-                    return;
-                }
-                // Get the Place object from the buffer.
-                final Place place = places.get(0);
-
-                // Format details of the place for display and show it in a TextView.
-                whatYouWant.setText("wertyuio" + place.getName());
-
-                // Display the third party attributions if set.
-                /*final CharSequence thirdPartyAttribution = places.getAttributions();
-                if (thirdPartyAttribution == null) {
-                    mPlaceDetailsAttribution.setVisibility(View.GONE);
-                } else {
-                    mPlaceDetailsAttribution.setVisibility(View.VISIBLE);
-                    mPlaceDetailsAttribution.setText(Html.fromHtml(thirdPartyAttribution.toString()));
-                }
-
-                Log.i(TAG, "Place details received: " + place.getName());
-                */
-
-                places.release();
-
-
-            }
-
-
-        };
-    }
     public class Dtask extends AsyncTask<String, String, String> {
 
 
